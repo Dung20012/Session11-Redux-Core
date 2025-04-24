421017b976bec7d68cd745b130e86f9b658c932f
Dung20012 <hantho1107@gmail.com>
2025-04-24 21:23:21 +0700

    Create Bai6v2.jsx

---
+++ Bai6v2.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateQuantity, removeFromCart } from '../reducers/cartReducer';
+
+const Cart = () => {
+  const cartItems = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="panel panel-danger">
+      <div className="panel-heading">
+        <h1 className="panel-title">Your Cart</h1>
+      </div>
+      <div className="panel-body">
+        {cartItems.length === 0 ? (
+          <div className="alert alert-warning text-center">
+            Empty product in your cart
+          </div>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th width="4%">STT</th>
+                <th>Name</th>
+                <th width="15%">Price</th>
+                <th width="4%">Quantity</th>
+                <th width="25%">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((item, index) => (
+                <tr key={item.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{item.name}</td>
+                  <td>{item.price} USD</td>
+                  <td>
+                    <input
+                      type="number"
+                      min="1"
+                      value={item.quantity}
+                      onChange={(e) => {
+                        const value = Math.max(1, parseInt(e.target.value || '1'));
+                        dispatch(updateQuantity({ id: item.id, quantity: value }));
+                      }}
+                      className="form-control"
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => dispatch(removeFromCart(item.id))}
+                      className="btn btn-danger"
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={4}>
+                  There are <b>{cartItems.length}</b> items in your shopping cart.
+                </td>
+                <td className="total-price text-left">
+                  {cartItems.reduce((total, item) => total + item.price * item.quantity, 0)} USD
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;

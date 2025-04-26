52c69a79e601954c0b1a07feccbbbb40074e3dd3
Dung20012 <hantho1107@gmail.com>
2025-04-26 19:47:43 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Cart from './shopping-cart/Cart'
+
+function App() {
+
+  return (
+    <>
+      <Cart />
+    </>
+  )
+}
+
+export default App
